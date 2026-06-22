37b7bb95996c3de2257af45b92e85ac824529d17
Jy1021XD <3116045079@qq.com>
2026-06-23 00:35:11 +0800

    dev update GitTest.java

--- src/main/java/org/exampl/gittest/GitTest.java
+++ src/main/java/org/exampl/gittest/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest
         System.out.println("Hello World1");
         System.out.println("Hello World2");
         System.out.println("Hello World3");
+        System.out.println("Hello World3");
+        System.out.println("Hello World3");
     }
 }
